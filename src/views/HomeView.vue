577ea365036c2305f08d70a5f34c7fe18ef974b7
DanielAD10 <adamsdaniel171@gmail.com>
2023-02-09 20:49:30 +0630

    fetch posts from db.json

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
     <h1>Home</h1> 
-    <PostsList :posts= "posts"></PostsList>
+    <div v-if="error">
+      {{error}}
+    </div>
+    <div v-if="posts.length>0">
+      <PostsList :posts= "posts"></PostsList>
+    </div>
+    <div v-else>
+      Loading .....
+    </div>
   </div>
 </template>
 
@@ -12,14 +20,25 @@ import { ref } from '@vue/reactivity'
 export default {
   components: { PostsList },
   setup() {
-    let posts = ref([
-      {
-        title: "Post 1", body: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error minima, provident ullam facilis nobis omnis natus deleniti similique tempora beatae ex perferendis accusantium. Pariatur esse, nihil veritatis illum facilis minus?", id: 1
-      },
-      {
-        title: "Post 2", body: "Something's waiting for me.", id: 2
+    let posts = ref([]);
+    let error = ref("");
+
+    let load = async() => {
+      // fetch data
+      try {
+        let response = await fetch("http://localhost:3000/posts");
+        if (response.status === 404) {
+          throw new Error("URL is wrong...");
+        }
+        let datas = await response.json();
+        posts.value = datas;
+      } catch (err) {
+        error.value = err.message;
       }
-    ])
+
+      
+    }
+    load();
 
     return {posts};
   }
