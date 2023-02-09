6646b1b3b26aa70e75df9ff3a0eab0a5608039ec
DanielAD10 <adamsdaniel171@gmail.com>
2023-02-09 19:48:12 +0630

    project init

---
+++ src/components/PostsList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-for="post in posts" :key="post.id">
+        <SinglePost :post= "post"></SinglePost>
+    </div>
+</template>
+
+<script>
+import { onUpdated } from '@vue/runtime-core'
+import SinglePost from './SinglePost'
+export default {
+  components: { SinglePost },
+    props: ["posts"],
+    setup() {
+        onUpdated(()=>{console.log("data updated")});
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1> 
+    <PostsList :posts= "posts"></PostsList>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostsList from '../components/PostsList'
+import { ref } from '@vue/reactivity'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  components: { PostsList },
+  setup() {
+    let posts = ref([
+      {
+        title: "Post 1", body: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error minima, provident ullam facilis nobis omnis natus deleniti similique tempora beatae ex perferendis accusantium. Pariatur esse, nihil veritatis illum facilis minus?", id: 1
+      },
+      {
+        title: "Post 2", body: "Something's waiting for me.", id: 2
+      }
+    ])
+
+    return {posts};
   }
 }
 </script>
