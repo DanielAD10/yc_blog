9446b003eecbe6abaad6c80c24fc14f377f36041
DanielAD10 <adamsdaniel171@gmail.com>
2023-04-09 03:25:43 +0630

    change firestore db config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBinNYan9cvzD5bNoGOJfYGkXswFXQlatI",
-    authDomain: "yc-blog-e800f.firebaseapp.com",
-    projectId: "yc-blog-e800f",
-    storageBucket: "yc-blog-e800f.appspot.com",
-    messagingSenderId: "102729350954",
-    appId: "1:102729350954:web:7c825c98e9833fe1a42853"
-};
+    apiKey: "AIzaSyBWNEFHtbjh3PVt_OLfsO0PpZVgSB0B704",
+    authDomain: "yc-blog-628e8.firebaseapp.com",
+    projectId: "yc-blog-628e8",
+    storageBucket: "yc-blog-628e8.appspot.com",
+    messagingSenderId: "1071785387577",
+    appId: "1:1071785387577:web:760b17c0c8ae304b838fe8"
+  };
 
 // init firebase
 firebase.initializeApp(firebaseConfig);
